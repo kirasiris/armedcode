f979c6fadd3f6c5161d576d80c03890d3e1c0e5b
Kevin Uriel Azuara Fonseca <kebin1421@hotmail.com>
2025-03-19 04:14:51 -0500

    Removed console.log() from handling membership functions

--- components/memberships/single.js
+++ components/memberships/single.js
@@ -13,11 +13,10 @@ const Single = ({ auth = {}, object = {} }) => {
 			if (!auth?.data?._id || !object?._id) return;
 
 			const res = await fetchurl(
-				`/subscribers?user=${auth.data._id}&resourceId=${object._id}&onModel=Membership`,
+				`/subscribers?user=${auth.data._id}&resourceId=${object._id}&onModel=Membership&isPaid=true`,
 				"GET",
 				"no-cache"
 			);
-			console.log("Verifying user enrollment", res);
 			setEnrollmentVerification(res);
 		};
 
@@ -34,7 +33,7 @@ const Single = ({ auth = {}, object = {} }) => {
 				status: "published",
 				onModel: "Membership",
 				isPaid: true,
-				website: process.env.NEXT_PUBLIC_WEBSITE_NAME,
+				website: process.env.NEXT_PUBLIC_WEBSITE_NAME, // THIS IS IMPORTANT FOR DB
 			}
 		);
 
@@ -52,7 +51,7 @@ const Single = ({ auth = {}, object = {} }) => {
 				status: "published",
 				onModel: "Membership",
 				isPaid: false,
-				website: process.env.NEXT_PUBLIC_WEBSITE_NAME,
+				website: process.env.NEXT_PUBLIC_WEBSITE_NAME, // THIS IS IMPORTANT FOR DB
 			}
 		);
 
@@ -72,64 +71,6 @@ const Single = ({ auth = {}, object = {} }) => {
 		window.location.reload();
 	};
 
-	// return (
-	// 	<div className="col-lg-6">
-	// 		<div className="card border border-1 my-border-color bg-black text-bg-dark mb-4">
-	// 			<div className="card-header">
-	// 				<h6>{object.title}</h6>
-	// 				<h3>
-	// 					{object.price.inHumanFormat}
-	// 					<small className="text-secondary">/month</small>
-	// 				</h3>
-	// 			</div>
-	// 			<div className="card-body">
-	// 				<ParseHtml text={object.text} parseAs="p" />
-	// 				{auth?.data?.isOnline ? (
-	// 					// If free and not enrolled
-	// 					(object?.data?.isFree && !enrollmentVerification?.sucess && (
-	// 						<button onClick={() => handleFreeEnrollment()}>
-	// 							Enroll for Free
-	// 						</button>
-	// 					)) ||
-	// 					// If not free and not enrolled
-	// 					(!object?.data?.isFree &&
-	// 						!enrollmentVerification?.sucess &&
-	// 						(auth.data.stripe.latestStripeCheckoutLink === null ||
-	// 						auth?.data?.stripe.latestStripeCheckoutLink === undefined ? (
-	// 							<button onClick={() => handlePaidEnrollment()}>
-	// 								Pay to Enroll
-	// 							</button>
-	// 						) : (
-	// 							<div className="btn-group">
-	// 								<a
-	// 									href={auth?.data?.stripe?.latestStripeCheckoutLink}
-	// 									target="_blank"
-	// 									className="btn btn-dark btn-sm w-100 mb-3"
-	// 								>
-	// 									There is a checkout session in your account!
-	// 								</a>
-	// 								<button onClick={() => handleCancellation()}>
-	// 									Cancel payment and Enrollment to Membership
-	// 								</button>
-	// 							</div>
-	// 						))) ||
-	// 					// If free/not free and already enrolled
-	// 					((object?.data?.isFree || !object?.data?.isFree) &&
-	// 						enrollmentVerification?.sucess && <p>Already enrolled</p>)
-	// 				) : (
-	// 					<a
-	// 						href={`${process.env.NEXT_PUBLIC_FOUNDER_WEBSITE_URL}auth/login?returnpage=${process.env.NEXT_PUBLIC_WEBSITE_URL}/api`}
-	// 						className="btn btn-light btn-sm w-100"
-	// 						target="_blank"
-	// 						rel="noreferrer noopener"
-	// 					>
-	// 						Sign In / Register to get Authentication Access
-	// 					</a>
-	// 				)}
-	// 			</div>
-	// 		</div>
-	// 	</div>
-	// );
 	return (
 		<div className="col-lg-6">
 			<div className="card border border-1 my-border-color bg-black text-bg-dark mb-4">
