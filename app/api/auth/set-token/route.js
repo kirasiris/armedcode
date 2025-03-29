23ba259800e0581deaae5d7e08c98348cbcb0a28
Kevin Uriel Azuara Fonseca <kebin1421@hotmail.com>
2025-03-29 17:31:57 -0500

    Trying to set secret_code in backend

--- app/api/auth/set-token/route.js
+++ app/api/auth/set-token/route.js
@@ -19,10 +19,6 @@ export async function GET(req) {
 	console.log("url token in route handler", urlToken);
 	console.log("secret_token in route handler", secret_token);
 
-	if (!urlToken) {
-		return NextResponse.json({ error: "Url token missing" }, { status: 400 });
-	}
-
 	// Redirect to a clean URL without token for security
 	const response = NextResponse.redirect(new URL("/api", req.url));
 
