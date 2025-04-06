1052fca015146548c6125c37aa348615aa35648f
Kevin Uriel Azuara Fonseca <kebin1421@hotmail.com>
2025-04-06 11:57:26 -0500

    Working on update form page

--- components/api/single.js
+++ components/api/single.js
@@ -14,7 +14,7 @@ const Single = ({ object = {} }) => {
 						</small>
 					</div>
 					<p className="mb-0">
-						{/* <Link
+						<Link
 							href={{
 								pathname: `/api/read/${object._id}`,
 								query: {},
@@ -23,8 +23,7 @@ const Single = ({ object = {} }) => {
 							<span className="badge rounded-pill text-bg-light me-2">
 								View
 							</span>
-						</Link> */}
-						<span className="badge rounded-pill text-bg-light me-2">View</span>
+						</Link>
 					</p>
 				</div>
 				<div className="card-body">
